381ae47497e729df41a2bcc4db56a600a7e6935a
LCHIN7X <lawchinxuan@gmail.com>
2025-06-04 17:09:38 +0800

    half

--- pln_d.cpp
+++ pln_d.cpp
@@ -13,4 +13,21 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>weight[i];
     }
+
+    sort(weight.begin(),weight.end());
+    double minWeight = weight[0];
+    double maxWeight = weight.back();
+
+    if (minWeight/maxWeight>p){
+        count<<0<<endl;
+        return 0;
+    }
+    int left =0;
+    int right = 500;
+
+    int answer = 500;
+    while(left <= right){
+        int mid = left + (right - left)/2;
+        if()
+    }
 }
