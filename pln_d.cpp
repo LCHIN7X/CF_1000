046d2d612e265adbe7530786008ab3c51273f8a8
LCHIN7X <lawchinxuan@gmail.com>
2025-06-04 16:39:33 +0800

    input

---
+++ pln_d.cpp
@@ -0,0 +1,16 @@
+//Destiny of Computer Science
+//PLN final round 2025
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double p; 
+    int n;
+    cin>>p>>n;
+
+    vector<int>weight(n);
+    for(int i=0;i<n;i++){
+        cin>>weight[i];
+    }
+}
